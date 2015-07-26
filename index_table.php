df008e8f549e4c809b5de645b2430eb8e543ca9e
thuytq <thuy.tq@hotmail.com>
2015-07-26 22:56:26 +0700

    check feed

--- index_table.php
+++ index_table.php
@@ -1,6 +1,5 @@
 <?php
-date_default_timezone_set('Asia/Bangkok');
-ob_start("zlib output compression");
+//ob_start("zlib output compression");
 session_start();
 //error_reporting(0);
 if (empty ($_SESSION["user_login"])) {
@@ -8,6 +7,7 @@ if (empty ($_SESSION["user_login"])) {
   echo "<script>window.location.href='login.php'</script>";
   exit;
 }
+
 include str_replace('\\', '/', dirname(__FILE__)) . '/content_spaw/spaw.inc.php';
 include str_replace('\\', '/', dirname(__FILE__)) . '/class/class.DEFINE.php';
 include str_replace('\\', '/', dirname(__FILE__)) . '/class/class.HTML.php';
@@ -19,11 +19,13 @@ include str_replace('\\', '/', dirname(__FILE__)) . '/class/simple_html_dom.php'
 include str_replace('\\', '/', dirname(__FILE__)) . '/class/class.BUSINESSLOGIC.php';
 include str_replace('\\', '/', dirname(__FILE__)) . '/class/template.php';
 include str_replace('\\', '/', dirname(__FILE__)) . '/Cache_Lite/Lite/Function.php';
+
 $html = SINGLETON_MODEL :: getInstance("HTML");
 $js = SINGLETON_MODEL :: getInstance("JAVASCRIPT");
 $css = SINGLETON_MODEL :: getInstance("CSS");
 $utl = SINGLETON_MODEL :: getInstance("UTILITIES");
 $dbf = SINGLETON_MODEL :: getInstance("BUSINESSLOGIC");
+
 $html->docType();
 $CONFIG = $dbf->loadSetting();
 if ($_GET['table_name'])
@@ -37,26 +39,37 @@ if ($_SESSION['table_name'] != '') {
       $arrayHeader["title"] = str_replace("{SESSION_USER}", $utl->stripUnicode($_SESSION['user_login']['fullname']), $arrayHeader["title"]);
   }
 }
-$options = array('cacheDir' => 'tmp/', 'lifeTime' => 3600);
-$cache = new Cache_Lite_Function($options);
+//$options = array('cacheDir' => 'tmp/', 'lifeTime' => 3600);
+//$cache = new Cache_Lite_Function($options);
 $html->openHTML();
 $html->openHead();
 $html->displayHead($arrayHeader["utf8"], $arrayHeader["refresh"], $arrayHeader["title"], $arrayHeader["keywords"], $arrayHeader["description"], $arrayHeader["copyright"], $arrayHeader["icon"]);
-$arrayCSS = array(pathTheme . "/style/style.pack.css", "css/ui.all.css", "style/jquery.Slidemenu.css", "js/fancybox/jquery.fancybox-1.3.1.css");
+/*
+$arrayCSS = array("themes/theme_default/style/style.pack.css", "css/ui.all.css", "style/jquery.Slidemenu.css", "js/fancybox/jquery.fancybox-1.3.1.css");
+
 foreach ($arrayCSS as $value)
+{
   $css->linkCSS($value);
-$html->closeHead();
-$html->openBody(array("div" => "divSwap"));
+}
+*/
+?>
+<link rel="stylesheet"  type="text/css" href="themes/theme_default/style/style.pack.css"/>
+<?php if(isset($_GET["insert"]) || isset($_GET["edit"])) {?>
+<link rel="stylesheet"  type="text/css" href="css/ui.all.css"/>
+<?php } ?>
+<link rel="stylesheet"  type="text/css" href="style/jquery.Slidemenu.css"/>
+<link rel="stylesheet"  type="text/css" href="js/fancybox/jquery.fancybox-1.3.1.css"/>
+<?php
+
 $arrayJS = array("js/jquery-1.3.2.min.js", "js/jquery.validate.pack.js", "js/adminLib.js", "js/ui.datepicker.js", "js/jquery.Slidemenu.js", "js/jquery.query.js", "js/fancybox/jquery.fancybox-1.3.1.pack.js");
 foreach ($arrayJS as $value)
   $js->linkJS($value);
-$URL_L = explode(".php", $_SERVER["REQUEST_URI"]);
-if ($URL_L[0] == '/hoadon') {
-  $action_f = "hoadon.php";
-}
-else {
-  $action_f = "";
-}
+
+$html->closeHead();
+$html->openBody(array());
+
+$action_f = "";
+
 $html->normalForm("frm", array("action" => $action_f, "method" => "post"));
 $dbf->showAdminHome($CURRENT_PAGE);
 $dbf->huy($CURRENT_PAGE . "?" . QUERY_STRING);
